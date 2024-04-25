16a074b9e74f37b4e31d1c2d81cf708f4fd0524d
Maksym Matsuhyria <maksymm@student.chalmes.se>
2024-04-25 21:15:44 +0200

    #27 Add functionality to append notifications

---
+++ Application/src/main/java/com/example/demo1/NotificationController.java
@@ -0,0 +1,37 @@
+package com.example.demo1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class NotificationController implements Initializable{
+
+    @FXML
+    private VBox notificationList;
+
+    @FXML
+    private Label testLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if(notificationList == null){
+            System.out.println("NotificationList is not intialized");
+        }
+    }
+
+    @FXML
+    public void addNotification(String message) {
+        Label newLabel = new Label(message);
+        newLabel.setStyle("-fx-font-size: 79;");
+        notificationList.getChildren().add(0, newLabel);
+    }
+
+    @FXML
+    public void test(String message) {
+        testLabel.setText(message);
+    }
+}
