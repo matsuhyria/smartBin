68dbfeceb46f43c0700fb7fc772cbfeecacc2b5f
Yaro <ursul@student.chalmers.se>
2024-04-26 14:29:45 +0200

    #27 Add screen switching functions (between Main and Notification pages

--- Application/src/main/java/com/example/demo1/BinAppController.java
+++ Application/src/main/java/com/example/demo1/BinAppController.java
@@ -1,7 +1,15 @@
 package com.example.demo1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class BinAppController {
 
@@ -10,6 +18,10 @@ public class BinAppController {
 
     @FXML
     private Label bin1_full;
+
+    private Stage stage = new Stage();
+    private Scene scene;
+    private Parent root;
 
     @FXML
     private void initialize() {
@@ -27,4 +39,13 @@ public class BinAppController {
         bin1_full.setText(newValue);
     }
 
+    public void switchToNotificationPage(ActionEvent event) throws IOException {
+
+        root = FXMLLoader.load(getClass().getResource("notificationPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- Application/src/main/java/com/example/demo1/NotificationController.java
+++ Application/src/main/java/com/example/demo1/NotificationController.java
@@ -1,12 +1,19 @@
 package com.example.demo1;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class NotificationController implements Initializable{
 
@@ -15,6 +22,10 @@ public class NotificationController implements Initializable{
 
     @FXML
     private Label testLabel;
+
+    private Stage stage = new Stage();
+    private Scene scene;
+    private Parent root;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -33,5 +44,15 @@ public class NotificationController implements Initializable{
     @FXML
     public void test(String message) {
         testLabel.setText(message);
+    }
+
+    public void switchToMainPage(ActionEvent event) throws IOException {
+
+        root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
