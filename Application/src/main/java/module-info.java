103ede5c4eae6f7173ec1ae77d921796f34c7a73
gulmuzv <gulmuz@student.chalmers.se>
2024-05-02 13:36:35 +0200

    #27 Add dependencies

--- Application/src/main/java/module-info.java
+++ Application/src/main/java/module-info.java
@@ -5,6 +5,4 @@ module com.example.demo1 {
     requires javafx.graphics;
     requires org.controlsfx.controls;
 
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1;
-}
+    opens com.example.demo1 to javafx.
