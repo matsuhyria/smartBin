a052b5c9f08c9f44e9fdf73ba2d317e3700e9e9b
Maksym Matsuhyria <maksymm@student.chalmers.se>
2024-05-10 20:56:15 +0200

    #34 Add FXMLpath unit tests

---
+++ Application/src/test/java/com/example/demo1/FXMLpathTest.java
@@ -0,0 +1,52 @@
+package com.example.demo1;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+
+public class FXMLpathTest{
+
+    @Test
+    public void testGetHeaderFxmlPath_ReturnsCorrectPath() {
+        FXMLpath path = FXMLpath.HEADER;
+        URL url = path.getFxmlPath();
+        assertNotNull(url);
+        assertTrue(url.getPath().contains("header.fxml"));
+    }
+
+    @Test
+    public void testGetBinCardFxmlPath_ReturnsCorrectPath() {
+        FXMLpath path = FXMLpath.BIN_CARD;
+        URL url = path.getFxmlPath();
+        assertNotNull(url);
+        assertTrue(url.getPath().contains("bin1.fxml"));
+    }
+
+    @Test
+    public void testGetMainPageFxmlPath_ReturnsCorrectPath() {
+        FXMLpath path = FXMLpath.MAIN_PAGE;
+        URL url = path.getFxmlPath();
+        assertNotNull(url);
+        assertTrue(url.getPath().contains("mainPage.fxml"));
+    }
+
+    @Test
+    public void testGetMapFxmlPath_ReturnsCorrectPath() {
+        FXMLpath path = FXMLpath.MAP_PAGE;
+        URL url = path.getFxmlPath();
+        assertNotNull(url);
+        assertTrue(url.getPath().contains("map.fxml"));
+    }
+
+    @Test
+    public void testGetNotificationFxmlPath_ReturnsCorrectPath() {
+        FXMLpath path = FXMLpath.NOTIFICATION_PAGE;
+        URL url = path.getFxmlPath();
+        assertNotNull(url);
+        assertTrue(url.getPath().contains("notificationPage.fxml"));
+    }
+
+}
