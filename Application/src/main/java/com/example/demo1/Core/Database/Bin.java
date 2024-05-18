4f21b948369b1c65dddcc5b117a6f106c6d17387
Victoria Yurevich <yurevich@student.chalmers.se>
2024-05-18 10:44:45 +0200

    #35 Add dark mode

--- Application/src/main/java/com/example/demo1/Core/Database/Bin.java
+++ Application/src/main/java/com/example/demo1/Core/Database/Bin.java
@@ -1,41 +1,41 @@
-package com.example.demo1.Core.Database;
+//package com.example.demo1.Core.Database;
 
 
-import javax.persistence.*;
+//import javax.persistence.*;
 
-@Entity
-@Table(name = "bins")
-public class Bin {
+//@Entity
+//@Table(name = "bins")
+//public class Bin {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    //private int id;
 
-    @Column(name = "x_coord")
-    private int xCoord;
+    //@Column(name = "x_coord")
+    //private int xCoord;
 
-    @Column(name = "y_coord")
-    private int yCoord;
+    //@Column(name = "y_coord")
+    //private int yCoord;
 
     // Getters and setters
 
-    public int getId() {
-        return id;
-    }
+    //public int getId() {
+        //return id;
+    //}
 
-    public int getXCoord() {
-        return xCoord;
-    }
+    //public int getXCoord() {
+        //return xCoord;
+    //}
 
-    public void setXCoord(int xCoord) {
-        this.xCoord = xCoord;
-    }
+    //public void setXCoord(int xCoord) {
+        //this.xCoord = xCoord;
+    //}
 
-    public int getYCoord() {
-        return yCoord;
-    }
+    //public int getYCoord() {
+        //return yCoord;
+    //}
 
-    public void setYCoord(int yCoord) {
-        this.yCoord = yCoord;
-    }
-}
+    //public void setYCoord(int yCoord) {
+        //this.yCoord = yCoord;
+    //}
+//}
--- Application/src/main/java/com/example/demo1/UI/SceneManager.java
+++ Application/src/main/java/com/example/demo1/UI/SceneManager.java
@@ -13,7 +13,10 @@ import com.example.demo1.Core.FXMLControllers.NotificationController;
 import com.example.demo1.Core.FXMLControllers.StatisticsController;
 import com.example.demo1.Util.Util;
 
+import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -21,6 +24,7 @@ public class SceneManager implements SceneSwitcher{
     private Stage stage;
     private final Pane rootPane = new Pane();
     private final Map<FXMLpath, Pane> sceneCache = new HashMap<>();
+    private boolean lightModeOn;
 
     private NotificationController notificationController;
     private CardController cardController;
@@ -34,16 +38,18 @@ public class SceneManager implements SceneSwitcher{
         mapController = mController;
         statController = sController;
         headerController = new HeaderController(this);
+        lightModeOn = true;
     }
 
     public void setStage(Stage stage, int height, int width) throws IOException{
+        initializeSceneCache();
         if(stage != null){
             this.stage = stage;
         } else{
             throw new IllegalArgumentException("Stage cannot be null");
         }
         
-        initializeSceneCache();
+        //initializeSceneCache();
 
         //Default starting page
         rootPane.getChildren().addAll(sceneCache.get(FXMLpath.MAIN_PAGE), sceneCache.get(FXMLpath.HEADER), sceneCache.get(FXMLpath.BIN_CARD)); //Main page (Bin overview page is opened by default)
@@ -90,6 +96,7 @@ public class SceneManager implements SceneSwitcher{
         sceneCache.put(FXMLpath.HEADER, header);
 
         Pane mainPage = Util.load(FXMLpath.MAIN_PAGE);
+        mainPage = setupModeChange(mainPage);
         sceneCache.put(FXMLpath.MAIN_PAGE, mainPage);
         
         Pane binCard = Util.load(FXMLpath.BIN_CARD, cardController);
@@ -103,6 +110,44 @@ public class SceneManager implements SceneSwitcher{
 
         Pane statPage = Util.load(FXMLpath.STATISTICS_PAGE, statController);
         sceneCache.put(FXMLpath.STATISTICS_PAGE, statPage);
+    }
+
+    private Pane setupModeChange(Pane pane){
+        Label label = (Label) pane.lookup("#changeMode");
+        label.setOnMouseEntered(event -> label.setStyle("-fx-text-fill: white;"));
+        label.setOnMouseExited(event -> label.setStyle(""));
+        label.setOnMouseClicked(event -> changeMode());
+        return pane;
+    }
+
+    public void changeMode() {
+        lightModeOn = !lightModeOn;
+
+        if (!lightModeOn) {
+            for (Pane pane : sceneCache.values()) {
+                pane.setStyle("-fx-background-color: #3A4952;"); // Dark mode
+                setNotificationPageStyle("#3A4952");
+            }
+        } else {
+            for (Pane pane : sceneCache.values()) {
+                pane.setStyle("-fx-background-color: #F9EFDB;"); // Light mode
+                setNotificationPageStyle("#F9EFDB");
+            }
+        }
+    }
+
+    private void setNotificationPageStyle(String color) {
+        Pane notificationPage = sceneCache.get(FXMLpath.NOTIFICATION_PAGE);
+        if (notificationPage != null) {
+            Node notificationList = notificationPage.lookup("#notificationList");
+            if (notificationList != null) {
+                notificationList.setStyle("-fx-background-color: " + color + ";");
+            }
+            Node notificationHorizontal = notificationPage.lookup("#notificationHorizontal");
+            if (notificationHorizontal != null) {
+                notificationHorizontal.setStyle("-fx-background-color: " + color + ";");
+            }
+        }
     }
 
     public Pane getCurrentPane(){
